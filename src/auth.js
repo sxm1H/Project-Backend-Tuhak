767d59f456dc54bba56a42a9a3cf8e7ee0ec97ac
Dilhan <z5419867@ad.unsw.edu.au>
2024-02-21 15:23:16 +1100

    Stubbed the AdminUserDetails function

--- src/auth.js
+++ src/auth.js
@@ -11,4 +11,14 @@ function adminAuthRegister(email, password, nameFirst, nameLast) {
       authUserId: 1,
     }
   }
-  
+function adminUserDetails(authUserId) {
+  return {
+      user: {
+          userId: 1,
+          name: 'Hayden Smith',
+          email: 'hayden.smith@unsw.edu.au',
+          numSuccessfulLogins: 3,
+          numFailedPasswordsSinceLastLogin: 1,
+      },
+  };
+}  
