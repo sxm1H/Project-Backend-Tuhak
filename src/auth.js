b8c9202fa5a2f0a4ba05b9519706ba3a25cd36d3
Dilhan <z5419867@ad.unsw.edu.au>
2024-02-21 15:32:25 +1100

    Stubbed the adminUserDetails function

--- src/auth.js
+++ src/auth.js
@@ -1,14 +1,24 @@
 // Sample stub for the authLoginV1 function
 // Return stub value matches table below
 function adminAuthLogin(email, password) {
-    return {
-      authUserId: 1,
-    }
+  return {
+    authUserId: 1,
   }
+}
 
 function adminAuthRegister(email, password, nameFirst, nameLast) {
-    return {
-      authUserId: 1,
-    }
+  return {
+    authUserId: 1,
   }
-  
+}
+function adminUserDetails(authUserId) {
+return {
+    user: {
+        userId: 1,
+        name: 'Hayden Smith',
+        email: 'hayden.smith@unsw.edu.au',
+        numSuccessfulLogins: 3,
+        numFailedPasswordsSinceLastLogin: 1,
+    },
+};
+}  
