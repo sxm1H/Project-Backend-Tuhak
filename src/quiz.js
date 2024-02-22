d0fb01b8808ed8ce1e2979adb12b38a8586c198a
Samuel <z5476869@vx06.orchestra.cse.unsw.EDU.AU>
2024-02-22 20:46:11 +1100

    finished stub function adminQuizList

--- src/quiz.js
+++ src/quiz.js
@@ -5,3 +5,14 @@ function adminQuizNameUpdate(authUserId, quizId, name) {
 function adminQuizRemove(authUserId, quizId) {
     return { } // Empty object
 }
+
+function adminQuizList(authUserId) {
+    return {
+        quizzes: [
+            {
+                quizId: 1,
+                name: 'My Quiz',
+            }
+        ]
+    }
+}
