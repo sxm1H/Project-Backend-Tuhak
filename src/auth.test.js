08ce14f9ae39dacdf8f6220f1b77af377eeea26f
Dun Yao <z5360233@ad.unsw.edu.au>
2024-02-29 21:51:33 +1100

    Finished writing tests for adminAuthRegister

--- src/auth.test.js
+++ src/auth.test.js
@@ -1,5 +1,4 @@
-import test from 'node:test';
-import { clear } from 'console';
+import { clear } from './other.js';
 import {
   adminAuthLogin, 
   adminAuthRegister,
@@ -9,6 +8,7 @@ import {
  } from './auth.js'
 
 describe('adminAuthRegister', () => {
+
   test('Successful registration', () => {
     clear();
     let user = adminAuthRegister('dunyao@unsw.edu.au', 'abcd1234', 'DunYao', 'Foo');
@@ -18,13 +18,14 @@ describe('adminAuthRegister', () => {
   test('Invalid email: Used by another user', () => {
     clear();
     let user1 = adminAuthRegister('dunyao@unsw.edu.au', 'abcd1234', 'DunYao', 'Foo');
-    let user2 = adminAuthRegister('dunyao@unsw.edu.au', 'bingbong', 'Nick', 'Sebastian');
-    expect(user2.error).toStrictEqual('Email is already used.');
+    let user2 = adminAuthRegister('dunyao@unsw.edu.au', '1234abcd', 'Nick', 'Sebastian');
+    expect(user2.error).toStrictEqual('Email is already in use.');
   });
 
   test.each([
     ['dunyaounsw.edu.au', 'abcd1234', 'DunYao', 'Foo'],
     ['dunyao@unsw', 'abcd1234', 'DunYao', 'Foo'],
+    ['dunyao@', 'abcd1234', 'DunYao', 'Foo'],
   ]) ('Invalid email: Incorrect input', (email, password, nameFirst, nameLast) => {
     clear();
     let user = adminAuthRegister(email, password, nameFirst, nameLast);
@@ -34,6 +35,7 @@ describe('adminAuthRegister', () => {
   test.each([
     ['dunyao@unsw.edu.au', 'abcd1234', '1984', 'Foo'],
     ['dunyao@unsw.edu.au', 'abcd1234', '?+-/*)(*&^%$#@!~`:><,.={}\|', 'Foo'],
+    ['dunyao@unsw.edu.au', 'abcd1234', 'Dun Yao123', 'Foo'],
   ]) ('Invalid first name: Characters', (email, password, nameFirst, nameLast) => {
     clear();
     let user = adminAuthRegister(email, password, nameFirst, nameLast);
@@ -41,18 +43,50 @@ describe('adminAuthRegister', () => {
   });
   
   test.each([
+    ['dunyao@unsw.edu.au', 'abcd1234', '', 'Foo'],
     ['dunyao@unsw.edu.au', 'abcd1234', 'o', 'Foo'],
     ['dunyao@unsw.edu.au', 'abcd1234', 'abcdefghijklmnopqrstu', 'Foo'],
+    ['dunyao@unsw.edu.au', 'abcd1234', 'abcdefghijklmnopqrstuv', 'Foo'],
   ]) ('Invalid first name: Length', (email, password, nameFirst, nameLast) => {
     clear();
     let user = adminAuthRegister(email, password, nameFirst, nameLast);
     expect(user.error).toStrictEqual('Invalid first name length.');
   });
   
-  test('Invalid password: Length', () => {
+  test.each([
+    ['dunyao@unsw.edu.au', 'abcd1234', 'DunYao', '1984'],
+    ['dunyao@unsw.edu.au', 'abcd1234', 'DunYao', '?+-/*)(*&^%$#@!~`:><,.={}\|'],
+    ['dunyao@unsw.edu.au', 'abcd1234', 'DunYao', 'Dun Yao123'],
+  ]) ('Invalid last name: Characters', (email, password, nameFirst, nameLast) => {
     clear();
-    let user = adminAuthRegister('dunyao@unsw.edu.au', 'abcd', 'DunYao', 'Foo');
-    expect(user.error).toStrictEqual('Password must be at least 8 characters.');
+    let user = adminAuthRegister(email, password, nameFirst, nameLast);
+    expect(user.error).toStrictEqual('Invalid characters in last name.');
+  });
+
+  test.each([
+    ['dunyao@unsw.edu.au', 'abcd1234', 'DunYao', ''],
+    ['dunyao@unsw.edu.au', 'abcd1234', 'DunYao', 'a'],
+    ['dunyao@unsw.edu.au', 'abcd1234', 'DunYao', 'abcdefghijklmnopqrstu'],
+    ['dunyao@unsw.edu.au', 'abcd1234', 'DunYao', 'abcdefghijklmnopqrstuv'],
+  ]) ('Invalid last name: Length', (email, password, nameFirst, nameLast) => {
+    clear();
+    let user = adminAuthRegister(email, password, nameFirst, nameLast);
+    expect(user.error).toStrictEqual('Invalid last name length.');
+  });
+  
+  test.each([
+    ['dunyao@unsw.edu.au', '', 'DunYao', 'Foo'],
+    ['dunyao@unsw.edu.au', 'a', 'DunYao', 'Foo'],
+    ['dunyao@unsw.edu.au', 'a1', 'DunYao', 'Foo'],
+    ['dunyao@unsw.edu.au', 'ab1', 'DunYao', 'Foo'],
+    ['dunyao@unsw.edu.au', 'ab12', 'DunYao', 'Foo'],
+    ['dunyao@unsw.edu.au', 'abc12', 'DunYao', 'Foo'],
+    ['dunyao@unsw.edu.au', 'abc123', 'DunYao', 'Foo'],
+    ['dunyao@unsw.edu.au', 'abcd123', 'DunYao', 'Foo'],
+  ]) ('Invalid password: Length', (email, password, nameFirst, nameLast) => {
+    clear();
+    let user = adminAuthRegister(email, password, nameFirst, nameLast);
+    expect(user.error).toStrictEqual('Password must be at least 8 characters long.');
   });
 
   test.each([
@@ -61,7 +95,7 @@ describe('adminAuthRegister', () => {
   ]) ('Invalid password: Characters', (email, password, nameFirst, nameLast) => {
     clear();
     let user = adminAuthRegister(email, password, nameFirst, nameLast);
-    expect(user.error).toStrictEqual('Password needs at least one number and at least one letter.');
+    expect(user.error).toStrictEqual('Password must have at least one number and one letter.');
   });
   
 });
