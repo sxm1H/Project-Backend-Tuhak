42192a16286f32f595c33fd4d119c854cdf8d5e5
Samuel <z5476869@vx12.orchestra.cse.unsw.EDU.AU>
2024-03-04 00:23:22 +1100

    fixed oversight in testing for adminQuizCreate

--- src/quiz.test.js
+++ src/quiz.test.js
@@ -9,49 +9,50 @@ beforeEach(() => {
 
 describe ("adminQuizCreate", () => {
 	test("User Id was not valid", () => {
+		let userId = adminAuthRegister("somethin@gmail.com", "validpassword123", "Franz", "Kafka");
 		let createQuiz1 = adminQuizCreate(-1, "amazing Quiz", "the quiz Id is not a number");
-		expect(createQuiz1).toStrictEqual({error: "Invalid quiz Id"});
+		expect(createQuiz1).toStrictEqual({error: "User Id is not valid"});
 		let createQuiz2 = adminQuizCreate(90000000, "better Quiz", "User id doesn't exist in the array");
-		expect(createQuiz2).toStrictEqual({error: "Invalid.quiz Id"});
+		expect(createQuiz2).toStrictEqual({error: "User Id is not valid"});
 	})
 	
-	test("Quiz name has invalid characters", () => {
+	test.only("Quiz name has invalid characters", () => {
 		let userId1 = adminAuthRegister("somethin@gmail.com", "validpassword123", "Franz", "Kafka");
-		let createQuiz1 = adminQuizCreate(userId1, "$:^)$", "Quiz smile");
+		let createQuiz1 = adminQuizCreate(userId1.authUserId, "$:^)$", "Quiz smile");
 		expect(createQuiz1).toStrictEqual({error: "Quiz Name contains invalid characters"});
-		let createQuiz2 = adminQuizCreate(userId1, "(^___^)@b", "Quiz Thumbsup");
+		let createQuiz2 = adminQuizCreate(userId1.authUserId, "(^___^)@b", "Quiz Thumbsup");
 		expect(createQuiz2).toStrictEqual({error: "Quiz Name contains invalid characters"});
-		let createQuiz3 = adminQuizCreate(userId1, "%%%%%%%%%", "percentage Quiz");
+		let createQuiz3 = adminQuizCreate(userId1.authUserId, "%%%%%%%%%", "percentage Quiz");
 		expect(createQuiz3).toStrictEqual({error: "Quiz Name contains invalid characters"});
 	})
 	
 	test("Check Quiz name is bettween valid character limit", () => {
 		let userId1 = adminAuthRegister("another@gmail.com", "validpassword12367", "Ludwig", "Beethoven");
-		let createQuiz1 = adminQuizCreate(userId1, "AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH",
+		let createQuiz1 = adminQuizCreate(userId1.authUserId, "AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH",
 		"Loud screaming is too long");
 		expect(createQuiz1).toStrictEqual({error:"Quiz Name must be more than 2 chracters and less than 31 characters long"});
-		let createQuiz2 = adminQuizCreate(userId1, "um", "Quiz is unsure");
+		let createQuiz2 = adminQuizCreate(userId1.authUserId, "um", "Quiz is unsure");
 		expect(createQuiz2).toStrictEqual({error:"Quiz Name must be more than 2 chracters and less than 31 characters long"});
-		let createQuiz3 = adminQuizCreate(userId1, " ", "Blank quiz");
+		let createQuiz3 = adminQuizCreate(userId1.authUserId, " ", "Blank quiz");
 		expect(createQuiz3).toStrictEqual({error:"Quiz Name must be more than 2 chracters and less than 31 characters long"});
 	})
 	
 	test("Check if Quiz Name already exists", () => {
 		let userId1 = adminAuthRegister("cool@gmail.com", "Thebestpassword123", "Isaac", "Newton");
-		let createQuiz1 = adminQuizCreate(userId1, "Cool Quiz", "The best quiz in the world");
-		let createQuiz2 = adminQuizCreate(userId1, "Cool Quiz", "Another cool quiz");
+		let createQuiz1 = adminQuizCreate(userId1.authUserId, "Cool Quiz", "The best quiz in the world");
+		let createQuiz2 = adminQuizCreate(userId1.authUserId, "Cool Quiz", "Another cool quiz");
 		expect(createQuiz2).toStrictEqual({error: "Quiz name already in use"});
 	})
 	
 	test("Quiz Description is too long", () => {
 		let userId1 = adminAuthRegister("Fake@gmail.com", "passwordpassword123", "first", "last");
-		let createQuiz1 = adminQuizCreate(userId1, "Too Long","Lorem ipsum dolor sit amet. Eos deleniti inventore est illo eligendi ut excepturi molestiae aut vero quas. Et dolorum doloremque ad reprehenderit adipisci qui voluptatem harum");
+		let createQuiz1 = adminQuizCreate(userId1.authUserId, "Too Long","Lorem ipsum dolor sit amet. Eos deleniti inventore est illo eligendi ut excepturi molestiae aut vero quas. Et dolorum doloremque ad reprehenderit adipisci qui voluptatem harum");
 		expect(createQuiz1).toStrictEqual({error: "Description is more than 100 characters"});
 	})
 	
 	test("Successful Quiz Created", () => {
 		let userId1 = adminAuthRegister("fakerT1@gmail.com", "pass123word", "Smith", "John");
-		let createQuiz1 = adminQuizCreate(userId1, "good name", "descriptive description");
+		let createQuiz1 = adminQuizCreate(userId1.authUserId, "good name", "descriptive description");
 		expect(createQuiz1.quizId).toBeDefined();
 		let createQuiz2 = adminQuizCreate(userId1, "blank quiz", "");
 		expect(createQuiz2.quizId).toBeDefined();
