3848e0426b9d1679558483c08ad8c7bd2cf4a8ed
Dun Yao <z5360233@ad.unsw.edu.au>
2024-03-05 20:32:43 +1100

    Created tests for adminQuizInfo

--- src/quiz.test.js
+++ src/quiz.test.js
@@ -210,4 +210,41 @@ describe ("adminQuizCreate", () => {
 	})    
 })
 
+describe('adminQuizInfo', () => {
 
+	let authUserId;
+	let quizId;
+	beforeEach(() => {
+		authUserId = adminAuthRegister('dunyao@unsw.edu.au', 'abcd1234', 'DunYao', 'Foo');
+		quizId = adminQuizCreate(authUserId, 'DunYao', 'lorem ipsum');
+	});
+
+	test('Success', () => {
+		expect(adminQuizInfo(authUserId, quizId)).toStrictEqual(
+			{
+				quizId: expect.any(Number),
+				name: expect.any(String),
+				timeCreated: expect.any(Number),
+				timeLastEdited: expect.any(Number),
+				description: expect.any(String),
+			}
+		);
+	});
+
+	test('Invalid AuthUserId', () => {
+		let info = adminQuizInfo(-2, quizId);
+		expect(info.error).toStrictEqual(expect.any(String));
+	});
+
+	test('Invalid quizId)', () => {
+		let info = adminQuizInfo(authUserId, -2);
+		expect(info.error).toStrictEqual(expect.any(String));
+	});
+
+	test('User does not own quiz', () => {
+		let authUserId2 = adminAuthRegister('sami@unsw.edu.au', '1234abcd', 'Sami', 'Hossain');
+		let info = adminQuizInfo(authUserId2, quizId);
+		expect(info.error).toStrictEqual(expect.any(String));
+	})
+	
+});
