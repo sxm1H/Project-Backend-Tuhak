a7b94835979d27abf05d61e43de837582bcf7b1c
Nicholas Sebastian <z5513422@ad.unsw.edu.au>
2024-03-08 20:59:42 +1100

    Fixed style for ALL files using eslint

--- src/quiz.js
+++ src/quiz.js
@@ -1,5 +1,5 @@
-import { isAlpha } from 'validator'
-import { getData, setData } from './dataStore.js'
+import { isAlpha } from 'validator';
+import { getData, setData } from './dataStore.js';
 let quizIdcounter = 0;
 
 /**
@@ -15,58 +15,58 @@ let quizIdcounter = 0;
 */
 function adminQuizNameUpdate(authUserId, quizId, name) {
 
-	let newdata = getData();
-	let userData = newdata.user;
-	let searchUserId = userData.findIndex(Ids => Ids.userId === authUserId);
-	let isAlphanumeric = /^[a-zA-Z0-9 ]+$/.test(name);
-	let date = Date.now();
+  let newdata = getData();
+  let userData = newdata.user;
+  let searchUserId = userData.findIndex(Ids => Ids.userId === authUserId);
+  let isAlphanumeric = /^[a-zA-Z0-9 ]+$/.test(name);
+  let date = Date.now();
 
-	if (searchUserId === -1) {
-		return {error: 'User Id is not valid'};
-	};
+  if (searchUserId === -1) {
+    return {error: 'User Id is not valid'};
+  }
 
-	if (!isAlphanumeric) {
-		return {error: 'Quiz Name contains invalid characters',}
-	};
+  if (!isAlphanumeric) {
+    return {error: 'Quiz Name contains invalid characters',};
+  }
 
-	if (name.length < 3 || name.length > 30 ) {
-		return {error: 'Quiz Name must be more than 2 chracters and less than 31 characters long'}
-	};
+  if (name.length < 3 || name.length > 30 ) {
+    return {error: 'Quiz Name must be more than 2 chracters and less than 31 characters long'};
+  }
 
-	let courseData = newdata.quizzes;
+  let courseData = newdata.quizzes;
 
-	for (let i of courseData) {
-		if (i.authUserId === authUserId) {
-			if (i.name === name) {
-				return {error: 'Quiz name already in use'}
-			}
-		}
-	}
+  for (let i of courseData) {
+    if (i.authUserId === authUserId) {
+      if (i.name === name) {
+        return {error: 'Quiz name already in use'};
+      }
+    }
+  }
 
-	let flag = 0;
-	for (let i = 0; i < newdata.quizzes.length; i++) {
+  let flag = 0;
+  for (let i = 0; i < newdata.quizzes.length; i++) {
     const data = newdata.quizzes[i];
     if (quizId === data.quizId) {
-        if (data.authUserId === authUserId) {
-            flag = 1;
-            data.name = name;
-						data.timeLastEdited = date;
-            break;
-        } else {
-            return {
-                error: 'Quiz ID does not refer to a quiz that this user owns.'
-            };
-        }
+      if (data.authUserId === authUserId) {
+        flag = 1;
+        data.name = name;
+        data.timeLastEdited = date;
+        break;
+      } else {
+        return {
+          error: 'Quiz ID does not refer to a quiz that this user owns.'
+        };
+      }
     }
-	}
+  }
 
-	if (!flag) {
-		return {
-			error: 'Quiz ID does not refer to a valid quiz',
-		}
-	}
+  if (!flag) {
+    return {
+      error: 'Quiz ID does not refer to a valid quiz',
+    };
+  }
 
-	return { } // Empty object
+  return { }; // Empty object
 }
 
 /**
@@ -80,46 +80,46 @@ function adminQuizNameUpdate(authUserId, quizId, name) {
 */
 function adminQuizRemove(authUserId, quizId) {
 
-	let newdata = getData();
+  let newdata = getData();
 
-	let flag = 0;
+  let flag = 0;
 
-	for (const data of newdata.user) {
-		if (authUserId === data.userId) {
-			flag = 1;
-			break;
-		}
-	}
+  for (const data of newdata.user) {
+    if (authUserId === data.userId) {
+      flag = 1;
+      break;
+    }
+  }
 
-	if (!flag) {
-		return {
-			error: 'authUserId is not a valid user.',
-		}
-	}
+  if (!flag) {
+    return {
+      error: 'authUserId is not a valid user.',
+    };
+  }
 
-	flag = 0;
-	for (let i = 0; i < newdata.quizzes.length; i++) {
+  flag = 0;
+  for (let i = 0; i < newdata.quizzes.length; i++) {
     const data = newdata.quizzes[i];
     if (quizId === data.quizId) {
-        if (data.authUserId === authUserId) {
-            flag = 1;
-            newdata.quizzes.splice(i, 1);
-            break;
-        } else {
-            return {
-                error: 'Quiz ID does not refer to a quiz that this user owns.'
-            };
-        }
+      if (data.authUserId === authUserId) {
+        flag = 1;
+        newdata.quizzes.splice(i, 1);
+        break;
+      } else {
+        return {
+          error: 'Quiz ID does not refer to a quiz that this user owns.'
+        };
+      }
     }
-}
+  }
 
-	if (!flag) {
-		return {
-			error: 'Quiz ID does not refer to a valid quiz',
-		}
-	}
+  if (!flag) {
+    return {
+      error: 'Quiz ID does not refer to a valid quiz',
+    };
+  }
 
-	return { } // Empty object
+  return { }; // Empty object
 }
 
 /**
@@ -131,23 +131,23 @@ function adminQuizRemove(authUserId, quizId) {
  * @return {Object {quizzes: Array}} - an Array of quizzes ojects that have quizId and name
  */
 function adminQuizList(authUserId) {
-  let newdata = getData()
+  let newdata = getData();
   let serachUserId = newdata.user.findIndex(ids => ids.userId === authUserId);
   
   if (serachUserId === -1) {
     return {
       error: 'invalid user Id'
-    }
+    };
   }
 
   let quizList =  newdata.quizzes.map(quizes => ({
     quizId: quizes.quizId,
     name: quizes.name,
-  }))
+  }));
 
-	return {
-		quizzes: quizList
-	}
+  return {
+    quizzes: quizList
+  };
 }
 
 /**
@@ -173,29 +173,29 @@ function adminQuizList(authUserId) {
   * } - Returns the quiz information user wants to access.
 */
 function adminQuizInfo(authUserId, quizId) {
-	let data = getData();
-	let searchUserId = data.user.findIndex(Ids => Ids.userId === authUserId);
-	let searchquizId = data.quizzes.findIndex(Ids => Ids.quizId === quizId);
+  let data = getData();
+  let searchUserId = data.user.findIndex(Ids => Ids.userId === authUserId);
+  let searchquizId = data.quizzes.findIndex(Ids => Ids.quizId === quizId);
 
-	if (searchUserId === -1) {
-		return { error: 'User Id is not valid.' };
-	} else if ( searchquizId === -1) {
-		return { error: 'Quiz Id is not valid.' };
-	}
+  if (searchUserId === -1) {
+    return { error: 'User Id is not valid.' };
+  } else if ( searchquizId === -1) {
+    return { error: 'Quiz Id is not valid.' };
+  }
 
-	let quizMatch = data.quizzes[searchquizId];
+  let quizMatch = data.quizzes[searchquizId];
 
-	if (authUserId !== quizMatch.authUserId) {
-		return { error: 'User does not own this quiz.' };
-	}
+  if (authUserId !== quizMatch.authUserId) {
+    return { error: 'User does not own this quiz.' };
+  }
 
-	return {
-		quizId: quizMatch.quizId,
-		name: quizMatch.name,
-		timeCreated: quizMatch.timeCreated,
-		timeLastEdited: quizMatch.timeLastEdited,
-		description: quizMatch.description,
-	}
+  return {
+    quizId: quizMatch.quizId,
+    name: quizMatch.name,
+    timeCreated: quizMatch.timeCreated,
+    timeLastEdited: quizMatch.timeLastEdited,
+    description: quizMatch.description,
+  };
 }
 
 /**
@@ -214,51 +214,51 @@ function adminQuizInfo(authUserId, quizId) {
  */
 
 function adminQuizCreate(authUserId, name, description) {
-	let newdata = getData();
-	let userData = newdata.user;
-	let searchUserId = userData.findIndex(Ids => Ids.userId === authUserId);
-	let isAlphanumeric = /^[a-zA-Z0-9 ]+$/.test(name);
-	let date = Date.now();
+  let newdata = getData();
+  let userData = newdata.user;
+  let searchUserId = userData.findIndex(Ids => Ids.userId === authUserId);
+  let isAlphanumeric = /^[a-zA-Z0-9 ]+$/.test(name);
+  let date = Date.now();
 
-	if (searchUserId === -1) {
-		return {error: 'User Id is not valid'};
-	};
+  if (searchUserId === -1) {
+    return {error: 'User Id is not valid'};
+  }
 
-	if (!isAlphanumeric) {
-		return {error: 'Quiz Name contains invalid characters',}
-	};
+  if (!isAlphanumeric) {
+    return {error: 'Quiz Name contains invalid characters',};
+  }
 
-	if (name.length < 3 || name.length > 30 ) {
-		return {error: 'Quiz Name must be more than 2 chracters and less than 31 characters long'}
-	};
+  if (name.length < 3 || name.length > 30 ) {
+    return {error: 'Quiz Name must be more than 2 chracters and less than 31 characters long'};
+  }
 
-	let courseData = newdata.quizzes;
+  let courseData = newdata.quizzes;
 
-	for (let i of courseData) {
-		if (i.authUserId === authUserId) {
-			if (i.name === name) {
-				return {error: 'Quiz name already in use'}
-			}
-		}
-	}
+  for (let i of courseData) {
+    if (i.authUserId === authUserId) {
+      if (i.name === name) {
+        return {error: 'Quiz name already in use'};
+      }
+    }
+  }
 
-	if (description.length > 100) {
-		return {error: 'Description is more than 100 characters'}
-	};
+  if (description.length > 100) {
+    return {error: 'Description is more than 100 characters'};
+  }
 
-	quizIdcounter++;
-	newdata.quizzes.push({
-		quizId: quizIdcounter,
-		name: name,
-		description: description,
-		authUserId: authUserId,
-		timeCreated: date,
-		timeLastEdited: date,
-	})
+  quizIdcounter++;
+  newdata.quizzes.push({
+    quizId: quizIdcounter,
+    name: name,
+    description: description,
+    authUserId: authUserId,
+    timeCreated: date,
+    timeLastEdited: date,
+  });
 
-	return {
-		quizId: quizIdcounter,
-	}
+  return {
+    quizId: quizIdcounter,
+  };
 }
 
 /**
@@ -291,46 +291,46 @@ function adminQuizCreate(authUserId, name, description) {
 */
 
 function adminQuizDescriptionUpdate(authUserId, quizId, description) {
-	let data = getData();
-	let date = Date.now();
+  let data = getData();
+  let date = Date.now();
 
-	if (data.user.findIndex(Ids => Ids.userId === authUserId) === -1) {
-		return {
-			error: 'Auth User ID invalid'
-		}
-	}
+  if (data.user.findIndex(Ids => Ids.userId === authUserId) === -1) {
+    return {
+      error: 'Auth User ID invalid'
+    };
+  }
 
-	let quizInfo = false;
+  let quizInfo = false;
 
-	for (let i = 0; i < data.quizzes.length; i++) {
-		if (data.quizzes[i].quizId === quizId) {
-			quizInfo = data.quizzes[i];
-			if (quizInfo.authUserId !== authUserId) {
-				return { error: 'Quiz Does Not Belong to User' }
-			}
-		}
-	}
+  for (let i = 0; i < data.quizzes.length; i++) {
+    if (data.quizzes[i].quizId === quizId) {
+      quizInfo = data.quizzes[i];
+      if (quizInfo.authUserId !== authUserId) {
+        return { error: 'Quiz Does Not Belong to User' };
+      }
+    }
+  }
 
-	if (quizInfo === false) {
-		return { error: 'Quiz ID Invalid' };
-	}
+  if (quizInfo === false) {
+    return { error: 'Quiz ID Invalid' };
+  }
 
-	if (description.length > 100) {
-		return { error: 'Description Too Long' };
-	} else {
-		quizInfo.description = description;
-		quizInfo.timeLastEdited = date;
-		return {};
-	}
+  if (description.length > 100) {
+    return { error: 'Description Too Long' };
+  } else {
+    quizInfo.description = description;
+    quizInfo.timeLastEdited = date;
+    return {};
+  }
 }
 
 
 export {
-	adminQuizNameUpdate,
-	adminQuizRemove,
-	adminQuizList,
-	adminQuizInfo,
-	adminQuizCreate,
-	adminQuizDescriptionUpdate,
-}
+  adminQuizNameUpdate,
+  adminQuizRemove,
+  adminQuizList,
+  adminQuizInfo,
+  adminQuizCreate,
+  adminQuizDescriptionUpdate,
+};
 
