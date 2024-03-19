d80608c2215ea8c9f95dd0a371fd3a2343211fea
Dun Yao <z5360233@ad.unsw.edu.au>
2024-03-19 18:01:13 +1100

    Changed tests for clear  to test http functions and renamed other.test.js to other.test.ts

---
+++ src/other.test.ts
@@ -0,0 +1,28 @@
+import { clear, adminAuthRegister } from './testHelpers';
+
+beforeEach(() => {
+  clear();
+});
+
+describe('Tests for clear', () => {
+
+  test('Successful login after clear of same id', () => {
+    expect(adminAuthRegister('nick@gmail.com', 'nick12345', 'Nicholas', 'Sebastian')).toStrictEqual(
+      {
+        statusCode: 200,
+        jsonBody: { authUserId: expect.any(Number) }
+      }
+    );
+
+    clear();
+
+    // Should expect id to be a number, since we made the same exact profile after clearing data.
+    expect(adminAuthRegister('nick@gmail.com', 'nick12345', 'Nicholas', 'Sebastian')).toStrictEqual(
+      {
+        statusCode: 200,
+        jsonBody: { authUserId: expect.any(Number) }
+      }
+    );
+  });
+  
+});
