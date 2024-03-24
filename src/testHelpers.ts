92fd1aaf9289d4bce49c16c33a58155f924887cf
Dun Yao <z5360233@ad.unsw.edu.au>
2024-03-24 19:39:30 +1100

    Added fake API for adminAuthLogout in testHelpers.ts

--- src/testHelpers.ts
+++ src/testHelpers.ts
@@ -107,6 +107,10 @@ const adminQuizDescriptionUpdate = (token: string, quizId: number, description: 
   return requestHelper('PUT', `/v1/admin/quiz/${quizId}/description`, { token, quizId, description });
 };
 
+const adminAuthLogout = (token: string) => {
+  return requestHelper('POST', '/v1/admin/auth/logout', { token } );
+};
+
 // ============================================================================================== //
 
 export {
