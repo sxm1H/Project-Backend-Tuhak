abd9541f61567afd075412f1fe68381ae34bc83e
z5479050 <z5479050@vx14.orchestra.cse.unsw.EDU.AU>
2024-03-25 00:34:30 +1100

    Finished tests for adminQuizQuestionDelete

--- src/adminQuizQuestionDelete.test.ts
+++ src/adminQuizQuestionDelete.test.ts
@@ -11,27 +11,34 @@ import {
     adminQuizRemove,
     adminQuizInfo,
     adminQuizNameUpdate,
-    adminQuizDescriptionUpdate
-  } from './testHelpers';
+    adminQuizDescriptionUpdate,
+    adminQuizQuestionDelete,
+    adminQuizQuestionCreate,
+} from './testHelpers';
 
-  beforeEach(() => {
-    clear();
-    const sessionId = adminAuthRegister('abcd.efgh@gmail.com', 'abcd1234', 'abcd', 'efgh').jsonBody;
-    const quizId = adminQuizCreate(sessionId.token, 'Australian Cities', 'lorem ipsum').jsonBody;
-    const questionId = adminQuizQuestionCreate(quizId.quizId, sessionId.token, 'Question1', 5, 5, [{answer: 'Melb', correct: true}, {answer: 'Syd', correct: false}]);
-  });
+beforeEach(() => {
+    clear();     
+});
 
   describe('Testing DELETE /v1/admin/quiz/:quizid/question/:questionid', () => {
+    let token: string;
+    let quizId: number;
+    let questionId: number;
+    beforeEach(() => {
+        token = adminAuthRegister('abcd.efgh@gmail.com', 'abcd1234', 'abcd', 'efgh').jsonBody.token;
+        quizId = adminQuizCreate(token, 'Australian Cities', 'lorem ipsum').jsonBody.quizId;
+        questionId = adminQuizQuestionCreate(quizId, token, 'Question1', 5, 5, [{answer: 'Melb', correct: true}, {answer: 'Syd', correct: false}]).jsonBody.questionId;
+    })
     test('Test Successful: Successfully Deleted a Question from a Quiz', () => {
-        expect(questionQuizQuestionDelete(sessionId.token, quizId.quizId, questionId.questionId)).toStrictEqual({
+        expect(adminQuizQuestionDelete(token, quizId, questionId)).toStrictEqual({
             jsonBody: {},
             statusCode: 200,
         })
     })
 
     test('Test Unsuccessful: User is not an owner of the quiz', () => {
-        const session2 = adminAuthRegister('glhfh@gmail.com', 'glhf', 'abcd', 'efgh').jsonBody;
-        expect(questionQuizQuestionDelete(session2.token, quizId.quizId, questionId.questionId)).toStrictEqual({
+        const session2 = adminAuthRegister('glhfh@gmail.com', 'glhf123111', 'abcd', 'efgh').jsonBody;
+        expect(adminQuizQuestionDelete(session2.token, quizId, questionId)).toStrictEqual({
             jsonBody: {
                 error: expect.any(String),
             },
@@ -43,7 +50,7 @@ import {
         [''],
         ['123123'],
     ])('Test Unsuccessful: Token Invalid', (token) => {
-        expect(questionQuizQuestionDelete(token, quizId.quizId, questionId.questionId)).toStrictEqual({
+        expect(adminQuizQuestionDelete(token, quizId, questionId)).toStrictEqual({
             jsonBody: {
                 error: expect.any(String),
             },
@@ -54,15 +61,12 @@ import {
     test.each([
         [-1],
         [123123],
-    ])('Test Unsuccessful: Token Empty', (questionId) => {
-        expect(questionQuizQuestionDelete(sessionId.token, quizId.quizId, questionId)).toStrictEqual({
+    ])('Test Unsuccessful: Invalid QuestionId', (questionId) => {
+        expect(adminQuizQuestionDelete(token, quizId, questionId)).toStrictEqual({
             jsonBody: {
                 error: expect.any(String),
             },
             statusCode: 400,
         })
     })
-
-
-
-  })
+})
