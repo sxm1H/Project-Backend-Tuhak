539c05dba9d14dd1fd10161bf24ffa724de9e02d
Nicholas Sebastian <z5513422@ad.unsw.edu.au>
2024-04-15 01:23:41 +1000

    Added four new working test files that expect thrown errors

--- src/iteration2Tests/adminUserDetails.test.ts
+++ src/iteration2Tests/adminUserDetails.test.ts
@@ -1,107 +1,84 @@
-// import {
-//   clear,
-//   adminAuthRegister,
-//   adminUserDetails,
-//   adminAuthLogin,
-// } from './testHelpersIter2';
+import {
+  clear,
+  adminAuthRegister,
+  adminUserDetails,
+  adminAuthLogin,
+} from './testHelpersIter2';
+import HTTPError from 'http-errors';
 
-// let token: string;
-// beforeEach(() => {
-//   clear();
+let token: string;
+beforeEach(() => {
+  clear();
 
-//   token = adminAuthRegister('Dilhanm@gmail.com', 'abCdddD123', 'Dilhan', 'Mert').jsonBody.token;
-// });
+  token = adminAuthRegister('Dilhanm@gmail.com', 'abCdddD123', 'Dilhan', 'Mert').token;
+});
 
-// describe('Testing GET /v1/admin/user/details', () => {
-//   test('Correct retrieves details', () => {
-//     const { statusCode, jsonBody } = adminUserDetails(token);
+describe('Testing GET /v1/admin/user/details', () => {
+  test('Correct retrieves details', () => {
+    expect(adminUserDetails(token)).toStrictEqual(
+      {
+        user: {
+          userId: expect.any(Number),
+          name: 'Dilhan Mert',
+          email: 'Dilhanm@gmail.com',
+          numSuccessfulLogins: 1,
+          numFailedPasswordsSinceLastLogin: 0,
+        }
+      }
+    );
+  });
 
-//     expect(statusCode).toStrictEqual(200);
-//     expect(jsonBody).toStrictEqual(
-//       {
-//         user: {
-//           userId: expect.any(Number),
-//           name: 'Dilhan Mert',
-//           email: 'Dilhanm@gmail.com',
-//           numSuccessfulLogins: 1,
-//           numFailedPasswordsSinceLastLogin: 0,
-//         }
-//       }
-//     );
-//   });
+  test('Invalid token', () => {
+    expect(() => adminUserDetails('nuhuhwrongwrong')).toThrow(HTTPError[401]);
+  });
 
-//   test('Invalid token', () => {
-//     expect(adminUserDetails('nuhuhwrongwrong')).toStrictEqual(
-//       {
-//         statusCode: 401,
-//         jsonBody: { error: expect.any(String) }
-//       }
-//     );
-//   });
+  test('Retrieves correct user details', () => {
+    const token2 = adminAuthRegister('DunYao@hotmail.com', 'abCdddD123', 'DunYao', 'Foo').token
 
-//   test('Retrieves correct user details', () => {
-//     const { jsonBody: reg2 } = adminAuthRegister('DunYao@hotmail.com', 'abCdddD123', 'DunYao', 'Foo');
-//     const token2 = reg2.token;
-//     const { statusCode, jsonBody } = adminUserDetails(token2);
+    expect(adminUserDetails(token2)).toStrictEqual({
+      user: {
+        userId: expect.any(Number),
+        name: 'DunYao Foo',
+        email: 'DunYao@hotmail.com',
+        numSuccessfulLogins: 1,
+        numFailedPasswordsSinceLastLogin: 0,
+      }
+    })
+  });
 
-//     expect(statusCode).toStrictEqual(200);
-//     expect(jsonBody).toStrictEqual(
-//       {
-//         user: {
-//           userId: expect.any(Number),
-//           name: 'DunYao Foo',
-//           email: 'DunYao@hotmail.com',
-//           numSuccessfulLogins: 1,
-//           numFailedPasswordsSinceLastLogin: 0,
-//         }
-//       }
-//     );
-//   });
+  test('Correct numSuccessfulLogins', () => {
+    for (let i = 0; i < 4; i++) {
+      adminAuthLogin('Dilhanm@gmail.com', 'abCdddD123');
+    }
 
-//   test('Correct numSuccessfulLogins', () => {
-//     for (let i = 0; i < 4; i++) {
-//       const { statusCode: login } = adminAuthLogin('Dilhanm@gmail.com', 'abCdddD123');
-//       expect(login).toStrictEqual(200);
-//     }
+    expect(adminUserDetails(token)).toStrictEqual({
+      user: {
+        userId: expect.any(Number),
+        name: 'Dilhan Mert',
+        email: 'Dilhanm@gmail.com',
+        numSuccessfulLogins: 5,
+        numFailedPasswordsSinceLastLogin: 0,
+      }
+    })
+  });
 
-//     const { statusCode, jsonBody } = adminUserDetails(token);
+  test('Correct numFailedPasswordsSinceLastLogin', () => {
+    for (let i = 0; i < 3; i++) {
+      expect(() => adminAuthLogin('Dilhanm@gmail.com', 'abCddddD1232')).toThrow(HTTPError[400]);
+    }
 
-//     expect(statusCode).toStrictEqual(200);
-//     expect(jsonBody).toStrictEqual(
-//       {
-//         user: {
-//           userId: expect.any(Number),
-//           name: 'Dilhan Mert',
-//           email: 'Dilhanm@gmail.com',
-//           numSuccessfulLogins: 5,
-//           numFailedPasswordsSinceLastLogin: 0,
-//         }
-//       }
-//     );
-//   });
+    expect(adminUserDetails(token)).toStrictEqual({
+      user: {
+        userId: expect.any(Number),
+        name: 'Dilhan Mert',
+        email: 'Dilhanm@gmail.com',
+        numSuccessfulLogins: 1,
+        numFailedPasswordsSinceLastLogin: 3,
+      }
+    })
 
-//   test('Correct numFailedPasswordsSinceLastLogin', () => {
-//     for (let i = 0; i < 3; i++) {
-//       const { statusCode: login } = adminAuthLogin('Dilhanm@gmail.com', 'abCddddD1232');
-//       expect(login).toStrictEqual(400);
-//     }
-
-//     const { statusCode, jsonBody } = adminUserDetails(token);
-
-//     expect(statusCode).toStrictEqual(200);
-//     expect(jsonBody).toStrictEqual(
-//       {
-//         user: {
-//           userId: expect.any(Number),
-//           name: 'Dilhan Mert',
-//           email: 'Dilhanm@gmail.com',
-//           numSuccessfulLogins: 1,
-//           numFailedPasswordsSinceLastLogin: 3,
-//         }
-//       }
-//     );
-//   });
-// });
+  });
+});
 
 test('temp', () => {
   expect(2 + 2).toBe(4);
